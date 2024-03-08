a4058906854e0ca455bc005f54c33939029622bf
XYF <xieyifeng@aliyun.com>
2024-03-08 21:09:43 +0800

    sadas

--- test.c
+++ test.c
@@ -5,5 +5,5 @@ int main()
     printf("Hello World!\n");  //输出
     printf("你好世界！\n");   //输出
     system("pause");    // 防止运行后自动退出，需头文件stdlib.h 
-    return 0;
+    return 0;asdihojsadhoiashjdias
 }
