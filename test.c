1c510a3a11127630d254a63bf160fd028d9dcc1f
zry <2044567452@qq.com>
2024-03-08 20:56:16 +0800

    T3

--- test.c
+++ test.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 int main()
 {
-    printf("Hello World!\n");
-    printf("你好世界！\n");   //输出汉字
+    printf("Hello World!\n");  //输出
+    printf("你好世界！\n");   //输出
     system("pause");    // 防止运行后自动退出，需头文件stdlib.h 
     return 0;
 }
